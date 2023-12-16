e7d8ebcbb810d7b47f8c8c36099cb3b151f4a877
Fedi Mbarek Abidi <121312608+fedimbarek@users.noreply.github.com>
2023-12-16 23:33:45 +0100

    Add files via upload

---
+++ app/produitpc/produitpc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produitpc',
+  templateUrl: './produitpc.component.html',
+  styleUrls: ['./produitpc.component.css']
+})
+export class ProduitpcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
